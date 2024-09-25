836870b693ead0277bf5f8f80465ae59b96cfc7f
scohen-trinity <scohen@trinity.edu>
2024-09-24 21:01:14 -0500

    feat: added a button to take a picture

--- lib/picture_page.dart
+++ lib/picture_page.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -36,8 +39,8 @@ class PicturePageState extends State<PicturePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder<void>(
+    return Scaffold(
+      body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
@@ -46,8 +49,44 @@ class PicturePageState extends State<PicturePage> {
             return const Center(child: CircularProgressIndicator());
           }
         }
-        
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            final image = await _controller.takePicture();
+
+            if (!context.mounted) return;
+
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            log(e.toString());
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({ super.key, required this.imagePath });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pic of your receipt')),
+      body: Image.file(File(imagePath)),
+      floatingActionButton: FloatingActionButton(onPressed: () { Navigator.pop(context); }),
     );
   }
 }
