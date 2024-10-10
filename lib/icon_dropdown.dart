7d71d83d8d412ce9dc5bf7ebf01f7a70269735db
scohen-trinity <scohen@trinity.edu>
2024-10-09 19:03:41 -0500

    feat: refactored icon dropdown for cleaner ui

--- lib/icon_dropdown.dart
+++ lib/icon_dropdown.dart
@@ -15,42 +15,88 @@ class IconDropdown extends StatefulWidget {
   const IconDropdown({super.key, this.setIcon});
 
   @override
-  _IcondDropdownState createState() => _IcondDropdownState();
+  State<IconDropdown> createState() { return _IcondDropdownState(); }
 }
 
 class _IcondDropdownState extends State<IconDropdown> {
-  List<IconData> icons = [Icons.account_balance_wallet, Icons.airplane_ticket, Icons.favorite];
+  final List<IconData> icons = [
+    Icons.account_balance_wallet,
+    Icons.airplane_ticket,
+    Icons.favorite,
+    Icons.home,
+    Icons.person,
+    Icons.map,
+    Icons.camera_alt,
+    ];
 
   final TextEditingController iconController = TextEditingController();
 
-  IconLabel? selectedIcon;
+  IconData? selectedIcon;
+
+  void _openIconGrid() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(10),
+          height: 250,
+          child: GridView.count(
+            crossAxisCount: 4,
+            shrinkWrap: true,
+            children: List.generate(icons.length, (index) {
+                return IconButton(
+                  icon: Icon(icons[index]),
+                  onPressed: () {
+                    setState(() {
+                      selectedIcon = icons[index];
+                      widget.setIcon?.call(Icon(selectedIcon));
+                    });
+                    Navigator.pop(context);
+                  },
+                );
+            }),
+          ),
+        );
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DropdownMenu<IconLabel>(
-      controller: iconController,
-      enableFilter: true,
-      requestFocusOnTap: true,
-      label: const Text('Icon'),
-      inputDecorationTheme: const InputDecorationTheme(
-        filled: true,
-        contentPadding: EdgeInsets.symmetric(vertical: 5.0),
-      ),
-      onSelected: (IconLabel? icon) {
-        setState(() {
-          selectedIcon = icon;
-          widget.setIcon?.call(selectedIcon!.icon);
-        });
-      },
-      dropdownMenuEntries: IconLabel.values.map<DropdownMenuEntry<IconLabel>>(
-        (IconLabel icon) {
-          return DropdownMenuEntry<IconLabel>(
-            value: icon,
-            label: icon.label,
-            leadingIcon: icon.icon,
-          );
-        },
-      ).toList(),
-    );
+    return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            selectedIcon != null ? Icon(selectedIcon, size: 100) : Text('No icon selected'),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _openIconGrid, child: Text('Select an icon'),
+            ),
+          ],
+        );
+    // return DropdownMenu<IconLabel>(
+    //   controller: iconController,
+    //   enableFilter: true,
+    //   requestFocusOnTap: true,
+    //   label: const Text('Icon'),
+    //   inputDecorationTheme: const InputDecorationTheme(
+    //     filled: true,
+    //     contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+    //   ),
+    //   onSelected: (IconLabel? icon) {
+    //     setState(() {
+    //       selectedIcon = icon;
+    //       widget.setIcon?.call(selectedIcon!.icon);
+    //     });
+    //   },
+    //   dropdownMenuEntries: IconLabel.values.map<DropdownMenuEntry<IconLabel>>(
+    //     (IconLabel icon) {
+    //       return DropdownMenuEntry<IconLabel>(
+    //         value: icon,
+    //         label: icon.label,
+    //         leadingIcon: icon.icon,
+    //       );
+    //     },
+    //   ).toList(),
+    // );
+  // }
   }
 }
